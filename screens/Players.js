65b551cf3770de3bfca69b9487dd4599ced0abeb
SAIDYYOUNES <younes.saidy63@gmail.com>
2024-02-25 17:33:31 +0100

    [KOOOR-12]Add Players screen component

---
+++ screens/Players.js
@@ -0,0 +1,45 @@
+import React, { useEffect } from 'react'
+import PlayerCard from '../components/PlayerCard'
+import { useSelector } from 'react-redux'
+import { View, FlatList, ActivityIndicator ,StyleSheet,TextInput} from 'react-native'
+import { useDispatch } from 'react-redux'
+import { getPlayers } from '../redux/players/actions'
+
+export default function Players() {
+  const dispatch = useDispatch()
+  const players = useSelector(state => state.player.players)
+
+  useEffect(() => {
+    dispatch(getPlayers())
+  }, [])
+  return (
+    <>
+   
+    {players.length>0 ? (
+      <View style={{ marginTop: 20 }}>
+        <FlatList
+          data={players}
+          renderItem={({ item }) => {
+            return <PlayerCard player={item} />
+          }}
+          ItemSeparatorComponent={() => <View style={{ height: 30 }} />}
+          keyExtractor={item => item.id}
+        />
+      </View>
+    ) : (
+      <View style={{ marginTop: 200 }}>
+
+        <ActivityIndicator size="large" />
+      </View>
+    )}
+    </>
+  )
+}
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
